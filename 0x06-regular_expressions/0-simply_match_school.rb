72551d013d00c3cf96948dc6f689ff88dfde2abf
root <tosinezekiel03@gmail.com>
2023-10-05 14:07:57 +0000

    Call me maybe

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
+# Matches School
 puts ARGV[0].scan(/Scho*l/).join
--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #Matches 10 digit phone number
-puts ARGV[0].scan(/(^\d{10,11})/).join
+puts ARGV[0].scan(/(^\d{10}$)/).join
