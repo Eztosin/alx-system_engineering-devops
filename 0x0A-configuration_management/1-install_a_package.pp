35daf3837aaa5ec9b84d9984c1b2b93f15ae8993
root <tosinezekiel03@gmail.com>
2023-11-25 09:22:10 +0000

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
-# Using Puppet to install flask from pip3
+# Using Puppet to install flask from pip
+
 $package = 'flask'
 
 package { $package:
     ensure   => '2.1.0',
-    provider => 'pip3',
+    provider => 'pip',
 }
