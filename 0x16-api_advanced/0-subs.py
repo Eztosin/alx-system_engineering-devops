d86d9273c80404321ce43fa3b6ae91668a21e06f
Eztosin <tosinezekiel03@gmail.com>
2024-01-17 19:22:54 +0000

    How many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,11 +2,12 @@
 """contains a function that queries the Reddit API"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers={"User-Agent":
-                                                "YourApp/1.0"})
+                                          "YourApp/1.0"})
 
     if response.status_code == 200:
         content = response.json()
