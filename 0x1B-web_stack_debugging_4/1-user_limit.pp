d8794d25886f5561552fcf3912629f91e1f611a5
Eztosin <tosinezekiel03@gmail.com>
2024-01-30 17:03:33 +0000

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,11 @@
 # Increase hard file limit for user holberton
-sudo sed -i '/holberton hard/s/5/50000/' /etc/security/limits.conf
+exec { 'increase-hard-file-limit-for-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
 
 # Increase soft file limit for user holberton
-sudo sed -i '/holberton soft/s/4/50000/' /etc/security/limits.conf
+exec { 'increase-soft-file-limit-for-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
